a2e8c7b4fddcf55ad56266021f71f17039998fe1
Noah Putnam <noahputnam@gmail.com>
2017-06-30 16:02:39 -0400

    added mario

---
+++ 3/skeleton/mario.py
@@ -0,0 +1,9 @@
+# Implement a program that prints out a half-pyramid of a specified height
+
+def main():
+  # remove this code and replace your own that acheives the task!
+  print("#", end="")
+  print(" ")
+
+if __name__ == "__main__":
+  main()
