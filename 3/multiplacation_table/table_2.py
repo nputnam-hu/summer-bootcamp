d6f23243b16a2f02fdfe760e7e28c4785cc016ae
Noah Putnam <noahputnam@gmail.com>
2017-06-30 15:54:26 -0400

    Update table_2.py

--- 3/multiplacation_table/table_2.py
+++ 3/multiplacation_table/table_2.py
@@ -3,18 +3,22 @@
 nums = [1,2,3,4,5,6,7,8,9,10]
 
 def main():
+  # print some space out
   print("     ", end="")
   for i in nums:
     print(" %2i " % (i), end="")
   enter()
   print("     ", end="")
+  # print row underneath to seperate
   for i in nums:
     print (" __ ", end ="")
   enter()
   for i in nums:
-    print(" %.2i| " % (i), end = "")
+    # print number on left
+    print(" %2i| " % (i), end = "")
     for j in nums:
-      print(" %.2i " % (i*j), end = "")
+      # print multiplacations
+      print(" %2i " % (i*j), end = "")
     enter()
 
 # mimicks enter key
