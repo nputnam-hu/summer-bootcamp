e9b3b980351375e73c54b4d60eb19292dffe1f29
Noah Putnam <noahputnam@gmail.com>
2017-07-12 01:16:50 -0400

    changed skeleton code

--- final_project/spam/skeleton/spam_classifier.py
+++ final_project/spam/skeleton/spam_classifier.py
@@ -6,56 +6,52 @@ program to classify how close a give string is to spam using NLTK, pulling strin
 import nltk.classify.util
 from nltk.classify import NaiveBayesClassifier
 from nltk.tokenize import word_tokenize
-import random
-import time
 import os
 
 from email_getter import get_emails
 import bootcamp
 
 # feeds a list of words into a dict where all the values map to True (important for NaiveBayesClassifier)
-def cast_todict(words):
+def cast_todict(words): 
+  """
+  example input->output:
+  ['apple', 'bannana', 'carrot'] -> {'apple':True,'bannana':True,'carrot':True}
+  ['Alice', 'Alice', 'Alice', 'Bob'] -> {'Alice':True,'Bob':True}
+  """
   return {}
-
-# clears the terminal screen using escape sequence
-def clear():
-  os.system("printf '\033c'")
 
 # loads the data in from the corpus/ dir into our classifier
 def load_data():
-  ham_emails = []
-  spam_emails = []
-  dircount = 0
-  filecount = 0
-  num_dirs = 285
-
   walkdir = os.path.abspath('corpus/')
+  # os.walk will recursively go through all of the files in the corpus directory
+  # and place them in the files variable
   for root, subdirs, files in os.walk(walkdir):
-    clear()
-    print('gathering training data...\nprocessed %i/%i directories, %i files' % (dircount, num_dirs, filecount)),
+    # if we're in the ham directory
     if 'ham' in os.path.abspath(root):
       for filename in files:      
-        filecount += 1
         with open(os.path.join(root, filename), encoding='latin-1') as f:
-          pass
+          # TODO: read data and load it into the ham_emails list
+          example_entry = ({}, 'ham')
+    # if we're in the spam directory
     if 'spam' in os.path.abspath(root):
       for filename in files:
-        filecount += 1
         with open(os.path.join(root, filename), encoding='latin-1') as f:
-          pass
-    dircount += 1
+          # TODO: read data and load it into the ham_emails list
+          example_entry = ({}, 'spam')
+
 
 
   return (ham_emails, spam_emails)
 
 def get_classifier():
   ham_emails, spam_emails = load_data()
-  total_emails = ham_emails + spam_emails
-  print('finished gathering data')
 
+  # TODO: combine the lists and randomly shuffle them
 
+  # TODO: partition the lists between training and test sets
+
+  # TODO: format everything correctly using word_tokenize and cast_todict
   training_set = []
-  print('creating classifier...')
   classifier = NaiveBayesClassifier.train(training_set)
   accuracy = nltk.classify.util.accuracy(classifier, test_set)
   print('Accuracy: ', accuracy * 100)
@@ -65,7 +61,7 @@ def get_classifier():
   return classifier
 
 def main():
-  pass
+  ## TODO: integrate everything
 
 if __name__ == '__main__':
   main()
